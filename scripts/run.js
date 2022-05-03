bbd3a1c6bf4401993d362b6aef4df7f6d8e3ee42
Nitish Kumar <nitishkr1729@gmail.com>
2022-05-03 21:58:08 +0530

    Storing Domain Data On Chain
    Signed-off-by: Nitish Kumar <nitishkr1729@gmail.com>

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,19 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract Deployed to:", domainContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let txn = await domainContract.register("doom");
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress("doom");
+    console.log("Owner of domain:", domainOwner);
+
+    txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
+    await txn.wait();
 };
 
 const runMain = async () => {
