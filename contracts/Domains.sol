29797dc3b3b78b475fce8bef34581cef9b6ac2ff
Nitish Kumar <nitishkr1729@gmail.com>
2022-05-03 21:36:43 +0530

    Basic Contract
    Signed-off-by: Nitish Kumar <nitishkr1729@gmail.com>

---
+++ contracts/Domains.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT-open-group
+
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+        console.log("THIS IS MY DOMAINS CONTRACT!!");
+    }
+}
