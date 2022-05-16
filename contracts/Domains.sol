9f43a3d48500ece90420763e54574c1261b9398b
Nitish Kumar <nitishkr1729@gmail.com>
2022-05-16 21:13:48 +0530

    Transacting Matic for providing domains
    Signed-off-by: Nitish Kumar <nitishkr1729@gmail.com>

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -2,19 +2,39 @@
 
 pragma solidity ^0.8.10;
 
+import { StringUtils } from "../libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
-    mapping(string => address) public domains;
+    string public tld;
 
+    mapping(string => address) public domains;
     mapping(string => string) public records;
 
-    constructor() {
-        console.log("THIS IS MY DOMAINS CONTRACT!!");
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("%s name service deployed", _tld);
     }
 
-    function register(string calldata name) public {
+    function price(string calldata name) public pure returns(uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+            return 5 * 10**17;
+        } else if (len == 4) {
+            return 3 * 10**17;
+        } else {
+            return 1 * 10**17;
+        }
+    }
+
+    function register(string calldata name) public payable {
         require(domains[name] == address(0));
+
+        uint _price = price(name);
+
+        require(msg.value >= _price, "Not enough Matic paid");
+
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
--- scripts/run.js
+++ scripts/run.js
@@ -1,19 +1,18 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("vibes");
     await domainContract.deployed();
+
     console.log("Contract Deployed to:", domainContract.address);
-    console.log("Contract deployed by:", owner.address);
 
-    let txn = await domainContract.register("doom");
+    let txn = await domainContract.register("chill", {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log("Owner of domain:", domainOwner);
+    const address = await domainContract.getAddress("chill");
+    console.log("Owner of domain chill:", address);
 
-    txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
-    await txn.wait();
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 };
 
 const runMain = async () => {
